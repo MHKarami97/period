6a63106b1510f797b89ffd068ee849ba236b61c0
mhkarami97 <mhkarami97@gmail.com>
2026-09-13 20:02:14 +0330

    fix

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -2,6 +2,6 @@ export * from "./database/AppDatabase";
 export * from "./repositories/DexieCycleRepository";
 export * from "./repositories/DexieSymptomRepository";
 export * from "./repositories/DexieProfileRepository";
-export * from "./pdf/PdfReportService";
+export * from "./pdf/PrintReportService";
 export * from "./backup/JsonBackupService";
 export * from "./backup/DatabaseResetService";
